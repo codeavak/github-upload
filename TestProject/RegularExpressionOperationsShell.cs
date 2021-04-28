c05f55951bfb23d41c525b7258320de348f68709
Johnny Avakian <javakian@taxcreditco.com>
2021-04-28 13:30:24 -0700

    initializing repository

--- TestProject/RegularExpressionOperationsShell.cs
+++ TestProject/RegularExpressionOperationsShell.cs
@@ -30,6 +30,9 @@ namespace TestProject
         public void IsMatch()
         {
             // Regex.IsMatch - Check if there is a match
+            string pattern = @"\d+";
+            string text = "42 is my lucky number";
+            Assert.IsTrue(Regex.IsMatch(text, pattern));
         }
 
         // Input Validation - Check if a text is integer
@@ -39,11 +42,49 @@ namespace TestProject
             // Using regex,  implement logic to check if the 
             // given text is an integer
             // use Regex.IsMatch
-
-            return false;
+            string pattern = @"^\d+$";
+            return Regex.IsMatch(text, pattern);
+            
         }
 
         [TestMethod]
+        public void IsIntegerTest()
+        {
+            string numStr = "1234";
+            Assert.IsTrue(IsInteger(numStr));
+            string notnumStr = "ababababa";
+            Assert.IsFalse(IsInteger(notnumStr));
+        }
+
+        [TestMethod]
+        public void IntegerUnitTest()
+        {
+            string[] passList = { "123", "456", "900", "0900" };
+            string[] failList = { "abc", "", ":", "ab92", "92ab","1 2 3",@"1\t2"};
+
+            //integers misclassified as non-integer
+            List<string> falseNegative = new List<string>();
+            //non-integers classified as integer
+            List<string> falsePositive = new List<string>();
+
+            foreach(string s in passList)
+            {
+                if (!IsInteger(s))
+                    falseNegative.Add(s);
+            }
+            foreach(string s in failList)
+            {
+                if (IsInteger(s))
+                    falsePositive.Add(s);
+            }
+
+            Assert.AreEqual(falseNegative.Count, 0);
+            Assert.AreEqual(falsePositive.Count, 0);
+
+            
+        }
+
+            [TestMethod]
         public void FirstMatch()
         {
             // Use Regex.Match to retrieve matching substring
@@ -65,6 +106,17 @@ namespace TestProject
         public void GroupsIndexed()
         {
             // Access Group using index
+            string pattern=@"(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})";
+            string text="your last visit date: 20210217";
+            var matches = Regex.Match(text,pattern);
+            var namesGroups = new string[] { "year", "month", "day" };
+            foreach(var name in namesGroups)
+            {
+                
+                
+                    Console.WriteLine($"{matches.Groups[name].Value}");
+                }
+            
         }
 
         [TestMethod]
@@ -78,6 +130,13 @@ namespace TestProject
         {
             // Use Regex.Replace method to find and replace
             // Reformat date YYYYMMDD => MM-DD-YYYY
+
+            string pattern = @"(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})";
+            string text = "your last visit date: 20210217";
+
+            string replacePattern = @"${month}-${day}-${year}";
+            var replaced=Regex.Replace(text, pattern, replacePattern);
+            Console.WriteLine(replaced);
         }
 
         [TestMethod]
